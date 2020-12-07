8bdd1f090dde8ce30c77b1ecfd794d5f60734a2e
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-12-07 21:02:34 +0100

    enable users follow and unfollow feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'users#index'
+  resources :users, only: [:index] do
+    member do
+      post :follow
+      post :unfollow
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
